032b429b407e69c840767e9781f39ba29097f61a
MAZYA <mazur097mazur@gmail.com>
2020-04-11 17:22:08 +0200

    11.04 -- step to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
-/* 
-    To start live watching:
-        1) In new project terminal do: npm i --save-dev gulp gulp-sass browser-sync
-        2) In terminal do: gulp watch
-*/
-
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');
+// const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
+
+gulp.task('imagemin', function() {
+    exports.default = () => (
+        gulp.src('./imgs/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('./imgs/'))
+)}
+);
 
 function style () {
     return gulp.src('./styles/scss/**/styles.scss') /* indicate where the .scss file is stored */
             .pipe(sass())
             .pipe(gulp.dest('./styles/css/'))
             .pipe(browserSync.stream())
+};
+
+/*gulp.task ('autoprefixer' ,function () {
+    return gulp.src('./styles/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['> 0.5%, last 2 versions, Firefox ESR, not dead'],
+            cascade: true
+        }))
+        .pipe(gulp.dest('app/css/'))
 }
+);*/
 
 function watch () {
     browserSync.init ({
@@ -24,7 +38,7 @@ function watch () {
     gulp.watch ('./styles/scss/**/*.scss', style);
     gulp.watch ('**/*.html').on('change', browserSync.reload);
 
-}
+};
 
 exports.style = style;
 exports.watch = watch;
