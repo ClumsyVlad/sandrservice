2db58a6ea8efb78e6c978cd50729502039e75ee6
MAZYA <mazur097mazur@gmail.com>
2020-04-21 18:13:13 +0200

    rebuilded + contacts + privacy template

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
     var ServicesSwiper = new Swiper('#services-slider', {
         speed: 400,
-        spaceBetween: 100,
+        spaceBetween: 30,
+        slidesPerView: 1,
         autoplay: {
             delay: 5000,    
         },
+        loop: true,
+        grabCursor: true 
     });
 
-    var ClientsSwiper = new Swiper('#clients-slider', {
-        speed: 400,
-        spaceBetween: 50,
-        slidesPerView: 3,
-        autoplay: {
-            delay: 5000,
-        }
-    });
+    // var ClientsSwiper = new Swiper('#clients-slider', {
+    //     speed: 400,
+    //     spaceBetween: 50,
+    //     slidesPerView: 3,
+    //     autoplay: {
+    //         delay: 5000,
+    //     },
+    //     loop: true,
+    //     grabCursor: true 
+    // });
 })
